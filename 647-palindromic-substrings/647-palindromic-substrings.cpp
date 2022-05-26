2d19a98a86a750de4606ad3f2a80b5b0636dd22f
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-05-26 19:52:19 +0530

    Time: 20 ms (48.21%), Space: 20.2 MB (21.59%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,23 +1,47 @@
 class Solution {
 public:
-    //Non Dynamic Programming Solution
     int countSubstrings(string s) {
-        int count = 0;
+        //Non Dynamic Programming Solution
+        
+        // int count = 0;
+        // int n = s.length();
+        // for(int i=0;i<n;i++){
+        //     int l = i,r=i;
+        //     //for odd length palindromes
+        //     while(l>=0 and r<n and s[l]==s[r]){
+        //         count++;
+        //         l--;
+        //         r++;
+        //     }
+        //     l=i;
+        //     r=i+1;
+        //     while(l>=0 and r<n and s[l]==s[r]){
+        //         count++;
+        //         l--;
+        //         r++;
+        //     }
+        // }
+        // return count;
+        
+        //Dynamic Programming Solution
+        
+        int count = 0 ;
         int n = s.length();
+        vector<vector<int>> dp(n,vector<int>(n,0));
         for(int i=0;i<n;i++){
-            int l = i,r=i;
-            //for odd length palindromes
-            while(l>=0 and r<n and s[l]==s[r]){
-                count++;
-                l--;
-                r++;
-            }
-            l=i;
-            r=i+1;
-            while(l>=0 and r<n and s[l]==s[r]){
-                count++;
-                l--;
-                r++;
+            dp[i][i]=1;
+            count++;
+        }
+        for(int col=1;col<n;col++){
+            for(int row=0;row<col;row++){
+                if(row==col-1 and s[row]==s[col]){
+                    dp[row][col]=1;
+                    count++;
+                }
+                if(s[row]==s[col] and dp[row+1][col-1]==1){
+                    dp[row][col]=1;
+                    count++;
+                }
             }
         }
         return count;
