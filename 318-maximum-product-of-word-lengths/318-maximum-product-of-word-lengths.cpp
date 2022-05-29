1158d75194a7dbab490a0e487df0495f87c6fd29
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-05-29 20:14:06 +0530

    Time: 163 ms (32.13%), Space: 21.5 MB (23.65%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        vector<string> count;
+        int n = words.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            string temp = string(26,'0'); 
+            for(int j=0;j<words[i].length();j++){
+                // cout<< words[i][j]-'a'<<" ";
+                temp[words[i][j]-'a']='1';
+            }
+            count.push_back(temp);
+            // cout<<temp<<endl;
+            // cout<<endl;
+        }
+        bool flag=true;
+        for(int i=0;i<n;i++){
+            for(int x=i+1;x<n;x++){
+                for(int j=0;j<26;j++){
+                    if(count[i][j]=='1' and count[x][j]=='1'){
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag){
+                    int temp=words[i].length()*words[x].length();
+                    ans =  max(ans,temp);
+                }
+                flag=true;
+            }
+            
+        }
+        return ans;
+        
+    }
+};
