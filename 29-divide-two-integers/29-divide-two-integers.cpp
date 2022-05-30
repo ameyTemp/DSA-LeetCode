3ef43d721d992b8a13e82ccb98d9ce6b38141fd1
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-05-30 14:47:45 +0530

    Time: 4 ms (49.70%), Space: 5.9 MB (76.09%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1) {
+        if(dividend==INT_MIN and divisor==-1)
             return INT_MAX;
-        }
-        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
-        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
-        while (dvd >= dvs) {
-            long temp = dvs, m = 1;
-            while (temp << 1 <= dvd) {
-                temp <<= 1;
-                m <<= 1;
+        long dvd = labs(dividend);
+        long dvs = labs(divisor);
+        long ans = 0;
+        int sign;
+        if((dividend>0 and divisor>0) or (dividend<0 and divisor<0))
+            sign=1;
+        else
+            sign=-1;
+        while(dvd>=dvs){
+            long temp = dvs;
+            int m = 1;
+            while(temp<<1<=dvd){
+                temp=temp<<1;
+                m=m<<1;
             }
-            dvd -= temp;
-            ans += m;
+            dvd-=temp;
+            ans+=m;
         }
-        return sign * ans;
+        return ans*sign;
     }
 };
