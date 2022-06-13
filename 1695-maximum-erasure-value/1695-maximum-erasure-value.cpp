68d7f6ff65062e62e5cd05c8d281884c5b787783
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-06-13 16:09:37 +0530

    Time: 212 ms (96.16%), Space: 96.4 MB (91.61%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
- int result = 0;
-        unordered_set<int> hset;
-        for (int i = 0, j = 0, win = 0; j < nums.size(); j++) {
-            while (hset.find(nums[j]) != hset.end()) {
-                hset.erase(nums[i]);
-                win -= nums[i];
-                i++;
-            }
-            hset.insert(nums[j]);
-            win += nums[j];
-            result = max(result, win);
+        vector<int> ind(10001,-1);
+        int n = nums.size();
+        vector<int> sums(n);
+        sums[0]=nums[0];
+        for(int i=1;i<n;i++){
+            sums[i]=sums[i-1]+nums[i];
+            // cout<<sums[i]<<endl;
         }
-        return result;
+        int sum = 0;
+        int maxsum=0;
+        int latest=-1;
+        for(int i=0;i<n;i++){
+            if(ind[nums[i]]>latest){
+                if(latest!=-1)
+                    sum+=sums[latest];
+                latest=ind[nums[i]];
+                sum-=sums[ind[nums[i]]];
+            }
+            sum+=nums[i];
+            ind[nums[i]]=i;
+            maxsum=max(sum,maxsum);
+            // cout<<sum<<" "<<maxsum<<" "<<nums[i]<<endl;
+        }
+        return maxsum;
     }
 };
