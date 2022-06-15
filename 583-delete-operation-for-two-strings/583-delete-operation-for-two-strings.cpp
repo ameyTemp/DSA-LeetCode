942022b9b89177e9ee2a16a175307ccb00d484d5
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-06-15 16:14:07 +0530

    Time: 34 ms (42.23%), Space: 11.5 MB (57.11%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -3,18 +3,21 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-        vector<vector<int>> arr(n+1,vector<int>(m+1,0));
+        vector<int> dp(m+1);
         for(int i=0;i<=n;i++){
+            vector<int> temp(m+1);
             for(int j=0;j<=m;j++){
-                if(i==0 or j==0)
-                    continue;
-                if(word1[i-1]==word2[j-1]){
-                    arr[i][j] = 1 + arr[i-1][j-1];
+                if(i==0 or j==0){
+                    temp[j]=i+j;
+                }else if(word1[i-1]==word2[j-1]){
+                    temp[j]=dp[j-1];
                 }else{
-                    arr[i][j] = max(arr[i-1][j],arr[i][j-1]);
+                    temp[j]=1+min(temp[j-1],dp[j]);
                 }
             }
+            dp=temp;
         }
-        return m+n-2*arr[n][m];
+        
+        return dp[m];
     }
 };
