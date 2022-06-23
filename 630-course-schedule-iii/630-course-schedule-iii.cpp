1d6d277f24ae90c3c239f1736f3ed571c23e30ba
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-06-24 01:47:19 +0530

    Time: 1927 ms (5.04%), Space: 272.3 MB (5.68%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -5,9 +5,7 @@ public:
     }
     int scheduleCourse(vector<vector<int>>& courses) {
         if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
-            return a[1] < b[1];
-        });
+        sort(courses.begin(), courses.end(),comparator);
         priority_queue<int> q;
         int sum = 0;
         for(auto i : courses) {
