25095f262cf115804d6847021b4522a7d4ee7041
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-07-15 11:40:08 +0530

    Time: 14 ms (80.35%), Space: 10.5 MB (75.40%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -13,23 +13,15 @@ public:
     ListNode* oddEvenList(ListNode* head) {
         if(head==NULL or head->next==NULL)
             return head;
-        ListNode* even=new ListNode(head->val);
-        ListNode* odd=new ListNode(head->next->val);    
-        ListNode* temp1=even,*temp2=odd;
-        head=head->next->next;
-        int count=1;
-        while(head!=NULL){
-            if(count%2==0){
-                odd->next=new ListNode(head->val);
-                odd=odd->next;
-            }else{
-                even->next=new ListNode(head->val);
-                even=even->next;
-            }
-            head=head->next;
-            count++;
+        ListNode *even=head,*odd=head->next;
+        ListNode *oddhead=odd;
+        while(odd!=NULL and odd->next!=NULL){
+            even->next=odd->next;
+            even=even->next;
+            odd->next=even->next;
+            odd=odd->next;
         }
-        even->next=temp2;
-        return temp1;
+        even->next=oddhead;
+        return head;
     }
 };
