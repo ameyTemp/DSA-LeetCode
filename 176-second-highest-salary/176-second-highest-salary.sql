1ffccafa52f59dd9269fa4526138bb9d0822a0ca
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-07-23 04:14:02 +0530

    Time: 314 ms (37.78%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,6 +1,6 @@
 select IFNULL(
-    Null, 
     (select distinct Salary from Employee 
-     order by Salary desc limit 1 offset 1)
+     order by Salary desc limit 1 offset 1), 
+    NULL
  )
 as SecondHighestSalary
