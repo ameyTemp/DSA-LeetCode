cf4003b571973ac53f327357bf119af4eef0f90c
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-07-25 14:11:02 +0530

    Time: 2870 ms (5.03%), Space: 465.2 MB (5.18%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,67 +1,70 @@
 class Solution {
 public:
-    
-    //0 -> not visited
-    //1 -> visited 
-    //2 -> visited and processed
-    bool detectCycle_util(vector<vector<int>>& adjList,vector<int>& visited,int v){
-        if(visited[v]==1)
+    bool checkCycle(int v,vector<int> &visited,vector<vector<int>> &adj){
+        if(visited[v]==1){
             return true;
-        if(visited[v]==2)
+        }
+        if(visited[v]==2){
             return false;
-        
-        visited[v] = 1;
-        for(int i=0;i<adjList[v].size();i++){
-            if(detectCycle_util(adjList,visited,adjList[v][i]))
+        }
+        visited[v]=1;
+        for(int i:adj[v]){
+            if(checkCycle(i,visited,adj)){
                 return true;
+            }
         }
         visited[v]=2;
         return false;
     }
-    bool detectCycle(vector<vector<int>>& adjList,int len){
-        vector<int> visited(len,0);
-        for(int i=0;i<len;i++){
-            if(visited[i]==0){
-                if(detectCycle_util(adjList,visited,i))
-                    return true;
-            }
-        }
-        return false;
-    }
-    void dfs(stack<int>& s,vector<vector<int>>& adjList,vector<int>& visited,int len,int v){
-        
-        if(visited[v]==1)
+    
+    void dfs(int v,vector<vector<int>> adj,vector<int> &vis,stack<int> &s){
+        if(vis[v]==1){
             return;
-        for(int i=0;i<adjList[v].size();i++){
-            if(visited[adjList[v][i]]==0)
-                dfs(s,adjList,visited,len,adjList[v][i]);
         }
-        visited[v]=1;
-        s.push(v);
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = prerequisites.size();
-        vector<int> ans;
-        vector<vector<int>> adjList(numCourses);
         
-        for(int i=0;i<n;i++){
-            adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        for(int i:adj[v]){
+            if(vis[i]==0)
+                dfs(i,adj,vis,s);
         }
+        s.push(v);
+        vis[v]=1;
+    }
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
         
         vector<int> visited(numCourses,0);
         
-        stack<int> num;
-        if(detectCycle(adjList,numCourses)){
+        for(int i=0;i<prerequisites.size();i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        int flag=0;
+        for(int i=0;i<numCourses;i++){
+            if(visited[i]==0){
+                bool a=checkCycle(i,visited,adj);
+                if(a==true){
+                    flag=1;
+                    break;
+                }
+            }
+        }
+        vector<int> ans;
+        if(flag==1){
             return ans;
         }
         
-        for(int i=0;i<numCourses;i++)
-            dfs(num,adjList,visited,numCourses,i);
+        stack<int> s;
+        vector<int> vis(numCourses,0);
         
-        while(!num.empty()){
-            int t = num.top();
-            ans.push_back(t);
-            num.pop();
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==0){
+                dfs(i,adj,vis,s);
+            }
+        }
+        
+        while(s.size()!=0){
+            ans.push_back(s.top());
+            s.pop();
         }
         return ans;
     }
