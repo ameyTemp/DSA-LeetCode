e242d104745b02cd6f260faedb936513e29673be
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-08-11 19:02:52 +0530

    Time: 37 ms (42.55%), Space: 13.3 MB (5.76%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> arr;
+        for(int i=0;i<nums.size();i++){
+            arr[nums[i]]=i;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(arr[target-nums[i]]!=0 and arr[target-nums[i]]!=i){
+                return {i,arr[target-nums[i]]};
+            }
+        }
+        return {};
+    }
+    
+};
