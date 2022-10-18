735d0de8857db096367294a05303f63b347a6d0c
ameyTemp <105974581+ameyTemp@users.noreply.github.com>
2022-10-18 19:25:37 +0530

    Time: 10 ms (83.60%), Space: 7.4 MB (13.40%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
+        map<char,int> a,b;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            a[s[i]]++;
+            b[t[i]]++;
+        }
+        if(s.length()==t.length() && a==b)
             return true;
         return false;
     }
